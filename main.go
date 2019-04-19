f67c5a2395a862e0ed82440fdc3cf3206a118460
Josh <josh.windsor.mail@gmail.com>
2019-04-20 00:58:03 +0100

    Added some comments & multi-retire

--- main.go
+++ main.go
@@ -10,69 +10,99 @@ import (
 	"time"
 )
 
+//struct to send the opcode and its order
 type channelTransaction struct {
 	opcode, order int
 }
 
 func main() {
+	//create unidirectional quit and quit callback channels
 	quitChannel := make(chan bool)
 	quitCompleteChannel := make(chan bool)
+	//starts the main dispatch thread
 	go dispatch(quitChannel, quitCompleteChannel)
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("\nJosh Windsor (24008182) Opcode Processor")
-	fmt.Print("Press Q to quit")
 
+	fmt.Print("\nJosh Windsor (24008182) Opcode Processor")
+	fmt.Print("\nPress any key to quit")
+
+	//creates a reader to wait for an input
+	reader := bufio.NewReader(os.Stdin)
 	reader.ReadString('\n')
+
+	//stops the program
 	fmt.Print("\nHalting Processor")
+	//sends quit to main thread to process & retire remaining opcodes
 	quitChannel <- true
+	//waits for the quit to complete before ending the main thread
 	<-quitCompleteChannel
 	fmt.Println("\nProcessing Complete")
 }
 
 const (
-	opnum = 15
+	//how long to loop for debug purposes
+	opnum = 10000
 )
 
 func dispatch(quitChannel <-chan bool, quitCompleteChannel chan<- bool) {
-
+	//creates an input & output channel for the pipelines
 	inputChannel := make(chan channelTransaction)
 	outputChannel := make(chan channelTransaction)
 
-	threadsAvailable := runtime.NumCPU() - 1
+	//spawns the number of threads for the processor minus main & this thread
+	threadsAvailable := runtime.NumCPU() - 2
 	fmt.Print("\nThreads Available: ")
 	fmt.Print(threadsAvailable)
 	for i := 0; i < threadsAvailable; i++ {
 		go pipeline(inputChannel, outputChannel)
 	}
 
+	//creates some arrays for comparison
 	inputData := []channelTransaction{}
 	outputData := []channelTransaction{}
 	outputDataUnsorted := []channelTransaction{}
+	//incrementors for processing
 	opcodesSent := 0
 	opcodesRetired := 0
 	opcodesRecieved := 0
+
+	//tag for quit out break
+OuterLoop:
+	//loops for the max number for debug (this would be inf on actual system)
 	for opcodesSent < opnum {
+		//creates a random opcode to send
 		randChannelData := channelTransaction{rand.Intn(9), opcodesSent}
 		select {
+		//quits the thread and processes remaining opcodes
 		case <-quitChannel:
-			break
+			break OuterLoop
+		//sends the new opcode to a waiting thread
 		case inputChannel <- randChannelData:
+			//stores the sent opcode for checking later
 			inputData = append(inputData, randChannelData)
 			opcodesSent++
+		//listens for return from a pipeline to retire
 		case threadReturn := <-outputChannel:
+			//stores the returned opcode for checking
 			outputData = append(outputData, threadReturn)
 			outputDataUnsorted = append(outputDataUnsorted, threadReturn)
+			//sorts the array to retire in order
 			sort.Slice(outputData, func(i, j int) bool {
 				return outputData[i].order < outputData[j].order
 			})
-			if opcodesRetired == outputData[opcodesRetired].order {
+			//if the next ordered opcode is finished then retire it
+			for opcodesRetired == outputData[opcodesRetired].order {
 				fmt.Print("\nRetired: ")
 				fmt.Print(outputData[opcodesRetired])
 				opcodesRetired++
+				if opcodesRetired == opcodesRecieved {
+					break
+				}
 			}
 			opcodesRecieved++
+		default:
 		}
 	}
+
 	//finishes off the remaining opcodes that have been processed
 	for opcodesRecieved < opcodesSent {
 		select {
@@ -82,10 +112,13 @@ func dispatch(quitChannel <-chan bool, quitCompleteChannel chan<- bool) {
 			sort.Slice(outputData, func(i, j int) bool {
 				return outputData[i].order < outputData[j].order
 			})
-			if opcodesRetired == outputData[opcodesRetired].order {
+			for opcodesRetired == outputData[opcodesRetired].order {
 				fmt.Print("\nRetired: ")
 				fmt.Print(outputData[opcodesRetired])
 				opcodesRetired++
+				if opcodesRetired == opcodesRecieved {
+					break
+				}
 			}
 			opcodesRecieved++
 		default:
@@ -115,13 +148,13 @@ func dispatch(quitChannel <-chan bool, quitCompleteChannel chan<- bool) {
 func pipeline(inputChannel <-chan channelTransaction, outputChannel chan<- channelTransaction) {
 	for {
 		thread := <-inputChannel
+		fmt.Print("\nStarted: ")
+		fmt.Print(thread)
 		time.Sleep(time.Second * time.Duration(thread.opcode))
+		fmt.Print("\n  Ended: ")
+		fmt.Print(thread)
 		outputChannel <- thread
 	}
-}
-
-func retire(inputChannel <-chan channelTransaction, outputChannel chan<- channelTransaction) {
-
 }
 
 func formatOutput(inputData []channelTransaction, outputData []channelTransaction, outputDataUnsorted []channelTransaction) {
